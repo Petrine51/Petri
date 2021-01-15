3aea5cc1b7ed8b157a1c0e6cb4c6b98684154f07
Petrine51 <kerrpeter52@gmail.com>
2021-01-16 00:22:11 +1100

    ayo

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function enlargeImg(img) {
     img.style.transform = "scale(100)";
     img.style.transition = "transform 2s ease";
 }
+
+function redirect(body) {
+    body.style.transform = "scale(-1)";
+    body.style.transition = "transform 0.5s ease";
+}
